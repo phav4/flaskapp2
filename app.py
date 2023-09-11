7009b677ea5cd63f9057c0f6ec3263d2cdbf6f78
dphavour <divinephavor@gmail.com>
2023-09-11 23:47:51 +0100

    b

--- app.py
+++ app.py
@@ -2,47 +2,47 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# data as an in-memory list
+# Sample data as an in-memory list
 persons = []
 
-# function to find a person by name
+# Helper function to find a person by name
 
 
 def find_person_by_name(name):
     for person in persons:
         if person['name'] == name:
             return person
-        return None
+    return None
 
-#      Create Operations
+# CREATE operation
 
 
-@app.route('/api/person', method=['POST'])
+@app.route('/api/person', methods=['POST'])
 def create_person():
     data = request.get_json()
     if 'name' not in data:
-        return jsonify({'error': 'Name is requied'}), 400
+        return jsonify({'error': 'Name is required'}), 400
     new_person = {
         'name': data['name'],
         'age': data.get('age', None)
     }
-
     persons.append(new_person)
-    return jsonify({'message': 'person created successfully'}), 201
+    return jsonify({'message': 'Person created successfully'}), 201
+
+# READ operation
 
 
-#      Read Operation
-@app.route('/api/person/<string:name>', method=['GET'])
-def update_person(name):
+@app.route('/api/person/<string:name>', methods=['GET'])
+def read_person(name):
     person = find_person_by_name(name)
     if person:
         return jsonify(person)
-    return jsonify({'error': 'person not found'}), 404
+    return jsonify({'error': 'Person not found'}), 404
 
-#       Update Operation
+# UPDATE operation
 
 
-@app.route('api/person/<string:name>', method=['PUT'])
+@app.route('/api/person/<string:name>', methods=['PUT'])
 def update_person(name):
     person = find_person_by_name(name)
     if not person:
@@ -50,16 +50,17 @@ def update_person(name):
     data = request.get_json()
     if 'age' in data:
         person['age'] = data['age']
-        return jsonify({'message': 'Person update sucessfully'})
+    return jsonify({'message': 'Person updated successfully'})
+
+# DELETE operation
 
 
-#       Delete Operation
-@app.route('/api/person/<string:name>', method=['DELETE'])
+@app.route('/api/person/<string:name>', methods=['DELETE'])
 def delete_person(name):
     person = find_person_by_name(name)
     if not person:
-        return jsonify({'error': 'person not found'}), 404
-    person.remove(person)
+        return jsonify({'error': 'Person not found'}), 404
+    persons.remove(person)
     return jsonify({'message': 'Person deleted successfully'})
 
 
