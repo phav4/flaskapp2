b038cc39ca0da5185a46125c932292fbc1341bfd
dphavour <divinephavor@gmail.com>
2023-09-11 22:20:41 +0100

    finished CURD

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# data as an in-memory list
+persons = []
+
+# function to find a person by name
+
 
 def find_person_by_name(name):
     for person in persons:
@@ -9,7 +14,7 @@ def find_person_by_name(name):
             return person
         return None
 
-#       Operations
+#      Create Operations
 
 
 @app.route('/api/person', method=['POST'])
@@ -24,3 +29,39 @@ def create_person():
 
     persons.append(new_person)
     return jsonify({'message': 'person created successfully'}), 201
+
+
+#      Read Operation
+@app.route('/api/person/<string:name>', method=['GET'])
+def update_person(name):
+    person = find_person_by_name(name)
+    if person:
+        return jsonify(person)
+    return jsonify({'error': 'person not found'}), 404
+
+#       Update Operation
+
+
+@app.route('api/person/<string:name>', method=['PUT'])
+def update_person(name):
+    person = find_person_by_name(name)
+    if not person:
+        return jsonify({'error': 'Person not found'}), 404
+    data = request.get_json()
+    if 'age' in data:
+        person['age'] = data['age']
+        return jsonify({'message': 'Person update sucessfully'})
+
+
+#       Delete Operation
+@app.route('/api/person/<string:name>', method=['DELETE'])
+def delete_person(name):
+    person = find_person_by_name(name)
+    if not person:
+        return jsonify({'error': 'person not found'}), 404
+    person.remove(person)
+    return jsonify({'message': 'Person deleted successfully'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
